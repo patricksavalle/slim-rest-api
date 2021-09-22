1dfe94b6d0122a66e2398baf15f4e229fec86e8e
Patrick Savalle <patrick@patricksavalle.com>
2021-09-22 20:42:48 +0200

    Serve cached responses from this server too if no reverse proxy already did.

--- Middleware/Memcaching.php
+++ Middleware/Memcaching.php
@@ -21,8 +21,19 @@ class Memcaching
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next): ResponseInterface
     {
         assert($request->getMethod() === 'GET');
-        $return = $next($request, $response->withHeader("cache-control", "max-age=" . $this->expiration));
-        Memcache::set($request->getUri(), $response->getBody(), $this->expiration);
+
+        $cache = Memcache::get($request->getUri());
+        if ($cache === false) {
+            error_log( "memcache miss: ". $request->getUri());
+            $return = $next($request, $response->withHeader("cache-control", "max-age=" . $this->expiration));
+            Memcache::set($request->getUri(), $response->getBody(), $this->expiration);
+        } else {
+            // -------------------------------------------------------------------------------------------
+            // if the reverse proxy is installed correctly it will have served the cached response already
+            // and this won't hit, otherwise we can still serve it from this server
+            // -------------------------------------------------------------------------------------------
+            $return = $response->withJson($cache);
+        }
         return $return;
     }
 }
