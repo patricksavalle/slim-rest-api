2d9e3b59c6bff0c37773892612f0fd6a6b70eb9d
Patrick Savalle <patrick@patricksavalle.com>
2022-01-06 11:42:38 +0100

    Separated no-store and no-cache.

--- Middleware/NoStore.php
+++ Middleware/NoStore.php
@@ -8,11 +8,11 @@ namespace SlimRestApi\Middleware {
     use Psr\Http\Message\ResponseInterface;
     use Psr\Http\Message\ServerRequestInterface;
 
-    class NoCache
+    class NoStore
     {
         public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next): ResponseInterface
         {
-            return $next($request, $response)->withHeader("cache-control", "no-cache");
+            return $next($request, $response)->withHeader("cache-control", "no-store");
         }
     }
 }
