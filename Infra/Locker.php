132305e345318520d9260a27208b1263b007fbf0
Patrick Savalle <patrick@patricksavalle.com>
2022-11-04 22:22:08 +0100

    Removed authentication module. Rewritten Locker to use apcu instead of mysql.

--- Infra/Locker.php
+++ Infra/Locker.php
@@ -15,48 +15,24 @@ namespace SlimRestApi\Infra {
          */
         static public function stash($iterable, int $ttl = 10 * 60): string
         {
-            // to make this module self-contained we create our own DB on demand
-            // this gives some overhead every time a token is stored but not very much
-            if (Db::execute("CREATE TABLE IF NOT EXISTS tokens85df2
-                (
-                    hash               CHAR(32)  NOT NULL,
-                    json               JSON      NOT NULL,
-                    expirationdatetime TIMESTAMP NOT NULL,
-                    PRIMARY KEY (hash),
-                    INDEX ( expirationdatetime )
-                )")->rowCount() > 0) {
-                error_log("Table tokens85df2 created on demand by " . __CLASS__);
-            }
-
             $json = json_encode($iterable);
             if (empty($json)) {
                 throw new Exception;
             }
             $hash = Password::randomMD5();
-            if (Db::execute("INSERT INTO tokens85df2(hash, json, expirationdatetime) VALUES (:hash, :json, ADDDATE( NOW(), INTERVAL :ttl SECOND))",
-                    [
-                        ":hash" => $hash,
-                        ":json" => $json,
-                        ":ttl" => $ttl,
-                    ])->rowCount() == 0
-            ) {
-                throw new Exception;
+            if (apcu_add($hash, $json, $ttl)===false) {
+                throw new Exception("APCU failure! Low on memory?");
             }
             return $hash;
         }
 
         static public function unstash(string $hash)/* : mixed */
         {
-            // try to get the token from the database if still valid
-            $row = Db::execute("SELECT json FROM tokens85df2 WHERE hash = :hash AND NOW() < expirationdatetime", [":hash" => $hash])->fetch();
-            if (empty($row)) {
+            $json = apcu_fetch($hash);
+            if (empty($json)) {
                 throw new Exception("Invalid or expired token or link", 401);
             }
-            // remove the token from the database and remove expired tokens
-            if (Db::execute("DELETE FROM tokens85df2 WHERE hash = :hash OR NOW() > expirationdatetime", [":hash" => $hash])->rowCount() === 0) {
-                error_log("Could not delete expired tokens in " . __CLASS__);
-            }
-            return json_decode($row->json);
+            return json_decode($json);
         }
     }
 }
